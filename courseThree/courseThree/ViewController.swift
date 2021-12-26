215372f6dd14205f456d7424db9619e74fb771e9
YuriyKropotin <jora-kirishi@list.ru>
2021-12-26 17:29:52 +0300

    no message

---
+++ courseThree/courseThree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  courseThree
+//
+//  Created by Юрий Кропотин on 26.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
